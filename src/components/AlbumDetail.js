9188e58497c19f083d334e212522ea0b3f725ac2
bobby <bwuphan@gmail.com>
2017-04-27 13:02:30 -0700

    Add album image in AlbumDetail 7.46

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,21 +5,26 @@ import CardSection from './CardSection';
 
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -33,6 +38,20 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around',
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
   }
 };
 
